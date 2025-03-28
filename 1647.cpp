a2f2e7ba057ee3502b8556d5a1d4cbabbc43c40c
NoahFromWSL <noahhan0304@gmail.com>
2025-03-28 18:13:50 +0900

    1647, 1197

--- 1647.cpp
+++ 1647.cpp
@@ -4,19 +4,16 @@
 
 using namespace std;
 
-typedef pair<int, pair<int, int>> p;
-
-vector<p> edges;
-int parent[10001];
-int V, E;
+int N, M;
+vector<pair<int, pair<int, int>>> edges;
+int parent[100010];
 int cost;
+int cutMAX = -1;
 
 int find_root(int x)
 {
     if (x != parent[x])
-    {
         parent[x] = find_root(parent[x]);
-    }
     return parent[x];
 }
 
@@ -33,50 +30,44 @@ void union_root(int x, int y)
     }
 }
 
-void init()
-{
-    sort(edges.begin(), edges.end());
-    for (int i = 1; i <= V; i++)
-    {
-        parent[i] = i;
-    }
-}
-
 void kru()
 {
-    int cnt_num = 0;
+    int mst_cnt = 0;
     for (int i = 0; i < edges.size(); i++)
     {
-        int cur_cost = edges[i].first;
         int f = edges[i].second.first;
         int s = edges[i].second.second;
+        int cur_cost = edges[i].first;
+
         if (find_root(f) == find_root(s))
-        {
             continue;
-        }
-        union_root(f, s);
+
         cost += cur_cost;
-        cnt_num++;
-        if (cnt_num == (V - 1))
-        {
+        if (cutMAX < cur_cost)
+            cutMAX = cur_cost;
+
+        union_root(f, s);
+        mst_cnt++;
+
+        if (mst_cnt == N - 1)
             return;
-        }
     }
 }
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> V >> E;
-    for (int i = 0; i < E; i++)
+    cin >> N >> M;
+    for (int i = 0; i < M; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
         edges.push_back({c, {a, b}});
     }
+    for (int i = 1; i <= N; i++)
+        parent[i] = i;
 
-    init();
+    sort(edges.begin(), edges.end());
     kru();
-    cout << cost;
+
+    cout << cost - cutMAX;
 }
